620cb05e49388edb8c45507a510874ede3ea9fba
Ignacio <ignaciojaviermedici9@gmail.com>
2023-04-28 14:42:02 -0300

    Funciones

---
+++ parcial/parcial.py
@@ -0,0 +1,132 @@
+import os
+os.system("cls")
+#1 - >
+"""
+    Cacular los grados fahrenheit teniendo grados celsius
+
+    Parámetros:
+    Grados celsius (float): grados celsius a convertir
+
+
+    Devuelve:
+    float: los grados celsius convertidos en fahrenheits
+"""
+# def cambiar_a_fahrenheit(grados_celsius):
+#     return (grados_celsius * 9/5) + 32
+
+# grados_C = 0
+
+# print(cambiar_a_fahrenheit(grados_C))
+
+
+
+# 2 - >
+"""
+    Calcular el área de un círculo
+
+    Parámetros:
+    Radio (float): radio del círculo
+
+    Devuelve:
+    float: el área del círculo
+
+"""
+# def calcular_area_ciculo(radio):
+#     return radio**2 *pi
+
+# pi = 3.14159
+
+# radio = 5
+
+# print(calcular_area_ciculo(radio))
+
+
+# 3 ->
+"""
+    Calcular el porcentaje de descuento aplicado a un producto
+
+    Parámetros:
+    Precio_original (float): precio original del producto
+    Precio_descuento (float): precio con descuento del producto
+
+    Devuelve:
+    float: el porcentaje de descuento aplicado al producto
+"""
+# def calcular_porcentaje_de_descuento(precio_original, precio_descuento):
+#     return ((precio_original - precio_descuento) * 100) / 100
+
+# precio_original = 100
+# precio_descuento = 60
+
+
+
+# descuento = calcular_porcentaje_de_descuento(precio_original, precio_descuento)
+# print("Porcetaje de descuento aplicado es {}%".format(descuento))
+
+# 4 ->
+"""
+    Calcular el promedio de edades de una lista de edades
+
+    Parámetros:
+    Lista_edades (list): lista de edades
+
+    Devuelve:
+    float: el promedio de edades de la lista
+"""
+# def calcular_promedio_edad(lista_edades):
+#     return sum(lista_edades) / len(lista_edades)
+
+# lista_edades = [20,21,30,36,12,10,50,73,95]
+
+
+# promedio = calcular_promedio_edad(lista_edades)
+# print("El promedio de edades es de {}".format(promedio))
+
+
+# 5->
+"""
+    Determinar numeor primo
+
+    Parámetros:
+    Numero (int): numero a determinar si es primo
+
+    Devuelve:
+    bool: True si es primo, False si no lo es
+"""
+# def determinar_numero_primo(numero):
+#     for n in range(2,numero):
+#         if numero % n == 0:
+#             return False
+#     return True
+
+
+# numero = 17
+
+# if determinar_numero_primo(numero):
+#     print("El numero {} es primo".format(numero))
+# else:
+#     print("El numero {} no es primo".format(numero))
+
+# 6->
+
+"""
+    Calcular el area de un triangulo
+
+    Parámetros:
+    Base (float): base del triangulo
+    Altura (float): altura del triangulo
+
+    Devuelve:
+    float: el area del triangulo
+
+"""
+
+# def calcular_area_triangulo(base, altura):
+#     return (base * altura) / 2
+
+# base = 10
+# altura = 5
+
+# print("El area del triangulo es {}".format(calcular_area_triangulo(base, altura)))
+
+# 7->
